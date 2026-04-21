5d7cf5c7ac1581d948de4b703d460e521dd39315
Santiago <lscbenitez@gmail.com>
2026-04-21 20:29:38 -0300

    Criação das Migrations em Laravel

---
+++ IFindLaravel/database/migrations/2026_04_21_205718_cria_tabela_post.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 50);
+            $table->text('descricao')->nullable();
+            $table->text('imagemurl');
+            $table->foreignId('item_id')->constrained('item')->restrictOnDelete()->cascadeOnUpdate();
+            $table->foreignId('aluno_id')->constrained('aluno')->restrictOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post');
+    }
+};
---
+++ IFindLaravel/database/migrations/2026_04_21_232551_cria_tabela_comentario.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comentario', function (Blueprint $table) {
+            $table->foreignId('aluno_id')->constrained('aluno')->restrictOnDelete()->cascadeOnUpdate();
+            $table->foreignId('post_id')->constrained('post')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('nome_aluno', 100);
+            $table->text('texto')->nullable();
+            $table->primary(['aluno_id', 'post_id']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comentario');
+    }
+};
